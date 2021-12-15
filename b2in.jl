f7b14bee391b5ce98777933c28a94742664db8ef
Ayaka Usui <ayaka.usui@oist.jp>
2021-12-15 20:28:22 +0100

    Update b2in.jl

--- b2in.jl
+++ b2in.jl
@@ -1,14 +1,14 @@
 function b2in(vecmb::Vector{Int})
 
-    Msize = legnth(vecmb)
-    Np = sum(vecmb)
+    mbsize = legnth(vecmb)-1
+    Np = sum(vecmb[1:mbsize])
 
-    # Lm = Msize+1
-    Ln = Np + 1
+    # Lm = mbsize+1
+    # Ln = Np + 1
     indfk = 1
     include("pascaltriangle.jl") # define pascaltriangle(m,n)
 
-    for jj = 1:Msize-2 # take -2 out?
+    for jj = 1:mbsize
         for ii = 0:vecmb[jj]
 
             if ii == vecmb[jj]
@@ -16,7 +16,8 @@ function b2in(vecmb::Vector{Int})
             end
 
             sumparticles = sum(vecmb[1:jj-1]) # numnber of particles taken account so far
-            indfk = indfk + pascaltriangle(Msize-jj,Np-ii-sumparticles)
+            pascal = pascaltriangle(mbsize-jj+1,Np-ii-sumparticles+1) # pascalmax(m+1,n+1) = (n+m-1)!/n!(m-1)!
+            indfk = indfk +
 
         end
     end
