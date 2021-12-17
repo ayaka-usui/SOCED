6c1eaaeabca8605ec69a6f6a73b8ea660e5fa19e
Ayaka Usui <ayaka.usui@oist.jp>
2021-12-17 20:15:02 +0100

    Update epsilon.jl

--- epsilon.jl
+++ epsilon.jl
@@ -3,14 +3,15 @@ function epsilon(ii::Int,jj::Int,Msize0::Int,ksoc::Float64)
     energy0 = 0.0
 
     if ii == jj
-
-       energy0 = jj + 1/2
-
-    else
-        if iseven(ii)
-
-        end
+       energy0 = energy0 + jj + 1/2
     end
+
+    if isodd(ii) && isodd(jj)
+
+       energy0 = energy0 + 1im*ksoc/sqrt(2)*(-1)*(sqrt(ceil(jj/2))+1)
+
+    end
+
     return
 
 end
