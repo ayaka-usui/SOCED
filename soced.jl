f7743e795bf81860e2b0e5799bc70213ed6ecc76
Ayaka Usui <ayaka.usui@oist.jp>
2021-12-18 09:37:52 +0100

    Update soced.jl

--- soced.jl
+++ soced.jl
@@ -1,14 +1,14 @@
 using Arpack, SparseArrays, LinearAlgebra
 
-function Hsoc(Msize0::Int64,Np::Int64,ksoc::Float64,Omega::Float64)
+# define functions used here
+include("ades.jl")
+include("acre.jl")
+include("pascaltriangle.jl")
+include("in2b.jl")
+include("b2in.jl")
+include("nop.jl")
 
-    # define functions used here
-    include("ades.jl")
-    include("acre.jl")
-    include("pascaltriangle.jl")
-    include("in2b.jl")
-    include("b2in.jl")
-    include("nop.jl")
+function Hsoc(Msize0::Int64,Np::Int64,ksoc::Float64,Omega::Float64)
 
     Msize = Msize0*2
     matp = pascaltriangle(Msize,Np) # the size is Msize+1 times Np+1
@@ -27,6 +27,8 @@ function Hsoc(Msize0::Int64,Np::Int64,ksoc::Float64,Omega::Float64)
         for mm = 1:nn
 
             vecmbmm = in2b(mm,Msize,Np)
+            energyijsum = 0.
+            energyij = 0.
 
             for jj = 1:Msize
 
@@ -37,87 +39,31 @@ function Hsoc(Msize0::Int64,Np::Int64,ksoc::Float64,Omega::Float64)
 
                 for ii = 1:Msize
 
-                    energy0 = epsilon(ii,jj,Msize0,ksoc,Omega)
-                    if energy0 == 0 # energy is Int and zero if ii==jj
+                    energyij = epsilon(ii,jj,Msize0,ksoc,Omega)
+                    if energyij == 0 # energy is Int and zero if ii==jj
                        continue
                     end
 
                     vecmbnnij = acre(ii,vecmbnnj)
-                    if  vecmbnnij \= vecmbmm
+                    if  vecmbnnij[1:Msize] != vecmbmm[1:Msize]
                         continue
                     end
 
-
-
-
-
+                    energyijsum = energyijsum + energyij
 
                 end
 
             end
 
-Hsoc[mm,nn] = vecmbmm[1:Msize]' * vecmbnnijsum
+            Hsoc[mm,nn] = energyijsum
 
         end
 
     end
 
+    # since the Hamiltonian is elmitian
+    Hsoc = Hsoc + Hsoc' - diagm(diag(Hsoc))
+
+    return Hsoc
+
 end
-
-# since the Hamiltonian is elmitian
-Hsoc = Hsoc + Hsoc' - diagm(diag(Hsoc))
-
-
-
-
-
-
-
-
-# defining vectors
-n = 10
-a = Matrix(I, n, n)
-
-b = [1 0 0; 0 1 0; 0 0 1]
-c = zeros(10,1)
-d = ones(10,1)
-
-e = sparse(a)
-eigs(e)
-
-# defining functions
-function f(x)
-    return x + 1
-end
-
-# g(1) will fail, but g(1.0) will work
-function g(x::Float64)
-    return x+1
-end
-
-# h(1) will work, but h("1") will not
-function h(x::T) where {T <: Number}
-    return x+1
-end
-
-function main(x)
-    f(x)
-    #g(x)
-    h(x)
-
-    return nothing
-end
-
-main(1)
-
-@time main(1)
-
-using BenchmarkTools
-
-@benchmark main(1)
-
-using Plots
-
-a = rand(10,10)
-
-heatmap(a)
