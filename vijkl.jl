dde12a3bfd6a094065af377ecd66d62d15505103
Ayaka Usui <ayaka.usui@oist.jp>
2021-12-19 18:09:33 +0100

    Update vijkl.jl

--- vijkl.jl
+++ vijkl.jl
@@ -1,31 +1,26 @@
-# function gammahalf(nn::Int64)
-#
-#    loggamma = log(pi)/2 + sum(log.(1:2*nn)) - 2*nn*log(2) - sum(log.(1:nn))
-#    return exp(loggamma)
-#
-# end
-
-function Ixx(ii::Int64, jj::Int64 ,kk::Int64, ll::Int64)
-
-   for m1 = 1:floor(Int64,ii/2)
-       for m2 = 1:floor(Int64,jj/2)
-           for m3 = 1:floor(Int64,kk/2)
-               for m4 = 1:floor(Int64,ll/2)
-
-                   if isodd(ii+jj+kk+ll)
-                      return
-                   else # iseven(ii+jj+kk+ll)
-                      integral = 1/sqrt(2)/2^((ii+jj+kk+ll)/2-m1-m2-m3-m4)*gammahalf((ii+jj+kk+ll)/2-m1-m2-m3-m4)
-                   end
-
-               end
-           end
-       end
-   end
-
-end
-
 function Vijkl(ii::Int64,jj::Int64,kk::Int64,ll::Int64)
 
+    n1 = ii
+    n2 = jj
+    n3 = kk
+    n4 = ll
+
+    A1234 = 1
+
+    for m1 = 0:ceil(Int64,n1/2)
+        for m2 = 0:ceil(Int64,n2/2)
+            for m3 = 0:ceil(Int64,n3/2)
+                for m4 = 0:ceil(Int64,n4/2)
+
+                    if m1==m2==m3==m4
+                       continue
+                    end
+
+                    A1234 = A1234*(n4-2*m4)*(n4-2*m4-1)/(m4+1)*2*((n1+n2+n3+n4)/2-m1-m2-m3-m4)/(n1+n2+n3+n4-2m1-2m2-2m3-2m4)/(n1+n2+n3+n4-2m1-2m2-2m3-2m4-1)
+
+                end
+            end
+        end
+    end
 
 end
