d77fcced778909f8a41463904ad116922f525568
Ayaka Usui <ayaka.usui@oist.jp>
2021-12-21 07:28:51 +0100

    edited

--- cutMsizeEne.jl
+++ cutMsizeEne.jl
@@ -4,6 +4,22 @@ include("b2in.jl")
 
 function cutMsizeEne(Msize0::Int64, Np::Int64, Ene0::Float64)
 
-    
+    Msize = Msize0*2
+
+    matp = zeros(Int,Msize+1,Np+1);
+    pascaltriangle!(Msize,Np,matp) # the size is Msize+1 times Np+1
+    maxmatp = matp[Msize+1,Np+1] # the indices are m+1 and n+1 for N^m_ns
+
+    for nn = 1:maxmatp
+
+        vecmbnn = in2b(nn,Msize,Np)
+
+        Enenn = 
+
+    end
+
+
+
+
 
 end
---
+++ in2bEne.jl
@@ -0,0 +1,54 @@
+include("pascaltriangle.jl") # define pascaltriangle(m,n)
+
+function in2bEne(jj::Int64, Msize::Int64, Np::Int64)
+
+    # this function return the index of Fock state at the index of many body state
+    indfk = copy(jj)
+
+    matp = zeros(Int,Msize+1,Np+1);
+    pascaltriangle!(Msize,Np,matp) # the size is Msize+1 times Np+1
+    # note the indices are m+1 and n+1 for N^m_n
+
+    # unlike Ponomarev's way, I count the index of Fock state in the backwards
+    # so that "1" means |Np,0,0,...>.
+    indfk = matp[Msize+1,Np+1] + 1 - indfk # the indices are m+1 and n+1 for N^m_n
+
+    if indfk > matp[Msize+1,Np+1] # the indices are m+1 and n+1 for N^m_n
+       error("indfk larger than the maximum of N^M_N.")
+    end
+
+    indfk = indfk - 1
+    # vecmb = spzeros(Int64,Msize+1) #spzeros(Int64,Msize+1)
+
+    indM = Msize-1
+    indN = Np
+
+    Ene = 0
+
+    while true
+
+          if indN == 0
+             break
+          end
+
+          if indfk >= matp[indM+1,indN+1]
+
+             indfk = indfk - matp[indM+1,indN+1] # the indices are m+1 and n+1 for N^m_n
+             # vecmb[Msize-indM] = vecmb[Msize-indM] + 1
+             indN = indN - 1
+
+             Ene = Ene + ceil(Int64,Msize-indM)
+
+          else
+             indM = indM - 1
+          end
+
+    end
+
+    # the last element for the coefficient
+    # vecmb[Msize+1] = 1
+
+    # return vecmb
+    return Ene
+
+end
