e49f75a1b0f8f9559861791f838bc33d586a48c7
Ayaka Usui <ayaka.usui@oist.jp>
2022-01-14 10:34:55 +0100

    Update socedcutoff2.jl

--- socedcutoff2.jl
+++ socedcutoff2.jl
@@ -30,18 +30,22 @@ function Hsocfunccutoff!(indvec::Vector{Int64}, Msize0::Int64, Np::Int64, matp::
 
     # defines vectors and matrices
     Hsoc .= 0; #Hsoc = spzeros(ComplexF64,maxmatpcut,maxmatpcut); #spzeros(ComplexF64,maxmatp,maxmatp);
+    vecmbnn = spzeros(Int64,Msize+1)
+    vecmbnnj = spzeros(Int64,Msize+1)
+    vecmbnnij = spzeros(Int64,Msize+1)
+    vecmbmm = spzeros(Int64,Msize+1)
 
     # define a matrix for the Hamiltonian
     for nn = 1:maxmatpcut #maxmatp # parfor
 
-        vecmbnn = spzeros(Int64,Msize+1)
+        vecmbnn .= spzeros(Int64,Msize+1)
         in2b!(indvec[nn],Msize,Np,matp,vecmbnn) #vecmbnn = in2b(indvec[nn],Msize,Np,matp) #in2b(nn,Msize,Np)
         # energyij = 0.
 
         # free terms
         for jj = 1:Msize
 
-            vecmbnnj = spzeros(Int64,Msize+1)
+            vecmbnnj .= spzeros(Int64,Msize+1)
             ades!(jj,vecmbnn,vecmbnnj) #vecmbnnj = ades(jj,vecmbnn)
             if vecmbnnj[Msize+1] == 0 # go back if a|0> = 0
                continue
@@ -49,7 +53,7 @@ function Hsocfunccutoff!(indvec::Vector{Int64}, Msize0::Int64, Np::Int64, matp::
 
             for ii = 1:Msize
 
-                vecmbnnij = spzeros(Int64,Msize+1)
+                vecmbnnij .= spzeros(Int64,Msize+1)
                 acre!(ii,vecmbnnj,vecmbnnij) #vecmbnnij = acre(ii,vecmbnnj)
 
                 # energyij = epsilon(ii,jj,ksoc,Omega)
@@ -60,7 +64,7 @@ function Hsocfunccutoff!(indvec::Vector{Int64}, Msize0::Int64, Np::Int64, matp::
 
                 for mm = 1:nn
 
-                    vecmbmm = spzeros(Int64,Msize+1)
+                    vecmbmm .= spzeros(Int64,Msize+1)
                     in2b!(indvec[mm],Msize,Np,matp,vecmbmm) #vecmbmm = in2b(indvec[mm],Msize,Np)
 
                     if vecmbnnij[1:Msize] == vecmbmm[1:Msize]
@@ -89,7 +93,7 @@ function main2(gdown::Float64, gup::Float64, gdu::Float64, ksoc::Float64, Omega:
 
     # define cutoff of energy in Fock states
     Msize = Msize0*2
-    Enecutoff = Msize0*2 #Msize0 - 1 + Int64(Np/2)
+    Enecutoff = Msize0 - 1 + Int64(Np/2)
     matp = zeros(Int64,Msize+1,Np+1)
     pascaltriangle!(Msize,Np,matp) # the size is Msize+1 times Np+1
     # note the indices are m+1 and n+1 for N^m_n
@@ -114,12 +118,12 @@ function main2(gdown::Float64, gup::Float64, gdu::Float64, ksoc::Float64, Omega:
     if isapprox(ksoc,0)
        vecg = [gdown, gup, gdu]
        vecgind = sortperm(vecg)
-       vecg = vecg[vecgind]
+       vecg .= vecg[vecgind]
        gcorrected = zeros(Float64,3)
        gcorrected[1] = correctionint(Msize0,vecg[1],real(lambda[1]))
        gcorrected[2] = correctionint(Msize0,vecg[2],real(lambda[2]))
        gcorrected[3] = correctionint(Msize0,vecg[3],real(lambda[3]))
-       gcorrected = gcorrected[vecgind]
+       gcorrected .= gcorrected[vecgind]
        println("ksoc=0")
        return gcorrected, lambda
    else
