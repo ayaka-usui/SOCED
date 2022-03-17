526cf0c1ad9ab35f6861b3e526c999ee73fe1e6a
Ayaka Usui <ayaka.usui@oist.jp>
2022-03-17 21:13:58 +0100

    Update Hintfunccutoff2.jl

--- Hintfunccutoff2.jl
+++ Hintfunccutoff2.jl
@@ -93,95 +93,7 @@ function coefficientInttid(vecmbindnn::Vector{Int64},vecmbindmm::Vector{Int64},v
 
 end
 
-function coefficientInt!(vecmbindnn::Vector{Int64},vecmbindmm::Vector{Int64},vecmbindnn3::Vector{Int64},vecmbindmm3::Vector{Int64},vecindcoeff::Matrix{Float64},Np::Int64)
-
-    # vecmbindnn3 = zeros(Int64,2)
-    # vecmbindmm3 = zeros(Int64,2)
-
-    # a|n>= sqrt(n)|n-1>
-    # a^+|n>= sqrt(n+1)|n+1>
-
-    vecindcoeff .= 0.0 #zeros(Float64,3,2)
-    ind0 = 0
-    vecmbind0 = 0
-    # common = 0
-
-    vecmbindnn3 .= 0
-    vecmbindmm3 .= 0
-
-    for pp = 1:Np
-
-        # consider to edit when parfor is inplemented
-        if vecmbindnn[pp] == vecmbind0
-           continue
-        end
-
-        for qq = 1:Np
-
-            if vecmbindnn[pp] == vecmbindmm[qq]
-
-               vecmbindnn3 .= vecmbindnn[findall(x->x!=pp,1:Np)] # ll kk
-               vecmbindmm3 .= vecmbindmm[findall(x->x!=qq,1:Np)] # jj ii
-               common = vecmbindnn[pp]
-
-               element = 1.0
-
-               # coefficients of operators for ket
-               if vecmbindnn3[1] == vecmbindnn3[2] # a_{kk} a_{kk}
-                  if common == vecmbindnn3[1]
-                     element = sqrt(Np*(Np-1))*element
-                  else
-                     element = sqrt(2*1)*element
-                  end
-               else # vecmbindnn3[1] != vecmbindnn3[2] # a_{kk} a_{ll}
-                  if common == vecmbindnn3[1] || common == vecmbindnn3[2]
-                     element = sqrt(2)*element
-                  # else
-                     # element = 1.0*element
-                  end
-                  element = element*2 # a_{kk} a_{ll} + a_{ll} a_{kk}
-               end
-
-               # coefficients of operators for bra
-               if vecmbindmm3[1] == vecmbindmm3[2] # a^+_{ii} a^+_{ii}
-                  if common == vecmbindmm3[1]
-                     element = sqrt(2*3)*element
-                  else
-                     element = sqrt(1*2)*element
-                  end
-               else # vecmbindmm3[1] != vecmbindmm3[2] # a^+_{ii} a^+_{jj}
-                  if common == vecmbindmm3[1] || common == vecmbindmm3[2]
-                     element = sqrt(2)*element
-                  # else
-                     # element = 1.0*element
-                  end
-                  element = element*2 # a^+_{ii} a^+_{jj} + a^+_{jj} a^+_{ii}
-               end
-
-               # indices for Vijkl
-               ind1 = [vecmbindnn3[1]-1, vecmbindnn3[2]-1, vecmbindmm3[1]-1, vecmbindmm3[2]-1] # ii jj kk ll
-               sort!(ind1,rev=true)
-               ind2 = binomial(ind1[1]+3,4) + binomial(ind1[2]+2,3) + binomial(ind1[3]+1,2) + binomial(ind1[4],1) + 1
-
-               ind0 += 1
-               vecindcoeff[ind0,1] = ind2
-               vecindcoeff[ind0,2] = element
-
-               vecmbind0 = vecmbindnn[pp]
-
-               break
-
-            end
-
-        end
-    end
-
-    vecindcoeff[1,3] = ind0
-    # return vecindcoeff, ind0
-
-end
-
-function coefficientInt2(vecmbindnn::Vector{Int64},vecmbindmm::Vector{Int64},vecmbindnn3::Vector{Int64},vecmbindmm3::Vector{Int64},vecindcoeff::Matrix{Float64},Np::Int64)
+function coefficientInt2tid(vecmbindnn::Vector{Int64},vecmbindmm::Vector{Int64},vecmbindnn3::Vector{Int64},vecmbindmm3::Vector{Int64},vecindcoeff::Matrix{Float64},Np::Int64)
 
     # down down up for g12
 
@@ -251,11 +163,13 @@ function coefficientInt2(vecmbindnn::Vector{Int64},vecmbindmm::Vector{Int64},vec
         end
     end
 
-    return vecindcoeff, ind0
+    vecindcoeff[1,3] = ind0
+
+    return vecindcoeff
 
 end
 
-function coefficientInt3(vecmbindnn::Vector{Int64},vecmbindmm::Vector{Int64},vecmbindnn3::Vector{Int64},vecmbindmm3::Vector{Int64},vecindcoeff::Matrix{Float64},Np::Int64)
+function coefficientInt3tid(vecmbindnn::Vector{Int64},vecmbindmm::Vector{Int64},vecmbindnn3::Vector{Int64},vecmbindmm3::Vector{Int64},vecindcoeff::Matrix{Float64},Np::Int64)
 
     # down down up for g
 
@@ -271,7 +185,7 @@ function coefficientInt3(vecmbindnn::Vector{Int64},vecmbindmm::Vector{Int64},vec
     # common = 0
 
     if vecmbindnn[end] != vecmbindmm[end]
-       return vecindcoeff, ind0
+       return vecindcoeff
     end
 
     vecmbindnn3 .= vecmbindnn[1:2] # ll kk
@@ -298,9 +212,26 @@ function coefficientInt3(vecmbindnn::Vector{Int64},vecmbindmm::Vector{Int64},vec
     ind0 += 1
     vecindcoeff[ind0,1] = ind2
     vecindcoeff[ind0,2] = element
+    vecindcoeff[1,3] = ind0
 
-    return vecindcoeff, ind0
+    return vecindcoeff
 
+end
+
+function in2bindmixedspins(Msize0::Int64,Np::Int64,indvec2::Vector{Int64},maxmatp21::Int64,matp20::Vector{Int64},matp21::Vector{Int64},vecmbindnn2::Vector{Int64},vecmbindnn::Vector{Int64})
+   # indvec2[nn] = (nndown-1)*maxmatp21 + nnup
+   indketup = mod(indvec2[nn],maxmatp21)
+   if indketup != 0
+      indketdown = div(indvec2[nn],maxmatp21) + 1
+   else # indketup == 0
+      indketdown = div(indvec2[nn],maxmatp21)
+      indketup = maxmatp21
+   end
+   in2bind!(indketdown,Msize0,Np-1,matp20,vecmbindnn2)
+   vecmbindnn[1:Np-1] = vecmbindnn2[1:Np-1]
+   in2bind!(indketup,Msize0,1,matp21,vecmbindnn2)
+   vecmbindnn[Np:Np] = vecmbindnn2[1:1]
+   return vecmbindnn
 end
 
 function Hintfunccutoff2!(indvec::Vector{Int64}, indvec2::Vector{Int64}, Msize0::Int64, Np::Int64, matp::Matrix{Int64}, matp20::Matrix{Int64}, matp21::Matrix{Int64}, Hintdown::ST, Hintup::ST, Hintdu::ST) where ST<:(SparseMatrixCSC{Float64, Ti} where Ti<:Integer)
@@ -381,120 +312,74 @@ function Hintfunccutoff2!(indvec::Vector{Int64}, indvec2::Vector{Int64}, Msize0:
     indrow_Hint = Vector(indrow_Hint)
     indcolumn_Hint = Vector(indcolumn_Hint)
     element_Hint = Vector(element_Hint)
-    deleteat!(indrow_Hint, element_Hint .== 0)
-    deleteat!(indcolumn_Hint, element_Hint .== 0)
-    deleteat!(element_Hint, element_Hint .== 0)
+    deleteat!(indrow_Hint, element_Hint .== 0.0)
+    deleteat!(indcolumn_Hint, element_Hint .== 0.0)
+    deleteat!(element_Hint, element_Hint .== 0.0)
     Hintdown .= sparse(indrow_Hint,indcolumn_Hint,element_Hint,maxmatpcut+maxmatpcut2*2+maxmatpcut,maxmatpcut+maxmatpcut2*2+maxmatpcut)
 
     # define Hint for down down up
     println("time for down down up")
     maxmatp21 = matp21[Msize0+1,1+1]
-    vecmbindnn2 = zeros(Int64,Np)
-    vecmbindmm2 = zeros(Int64,Np)
+    vecmbindnn2tid = zeros(Int64,Np,tmax)
+    vecmbindmm2tid = zeros(Int64,Np,tmax)
+
+    indrow_Hint = SharedArray{Int64,1}(binomial(maxmatpcut2+1,2))
+    indcolumn_Hint = SharedArray{Int64,1}(binomial(maxmatpcut2+1,2))
+    element_Hint = SharedArray{Float64,1}(binomial(maxmatpcut2+1,2))
+
+    indrow_Hint2 = SharedArray{Int64,1}(binomial(maxmatpcut2+1,2))
+    indcolumn_Hint2 = SharedArray{Int64,1}(binomial(maxmatpcut2+1,2))
+    element_Hint2 = SharedArray{Float64,1}(binomial(maxmatpcut2+1,2))
 
     for nn = 1:maxmatpcut2 # parfor # down down up for ket
 
-        # ket
-        # indvec2[nn] = (nndown-1)*maxmatp21 + nnup
-        indketup = mod(indvec2[nn],maxmatp21)
-        if indketup != 0
-           indketdown = div(indvec2[nn],maxmatp21) + 1
-        else # indketup == 0
-           indketdown = div(indvec2[nn],maxmatp21)
-           indketup = maxmatp21
-        end
-        in2bind!(indketdown,Msize0,Np-1,matp20,vecmbindnn2)
-        vecmbindnn[1:Np-1] = vecmbindnn2[1:Np-1]
-        in2bind!(indketup,Msize0,1,matp21,vecmbindnn2)
-        vecmbindnn[Np:Np] = vecmbindnn2[1:1]
+        tid = Threads.threadid()
 
-        # a = findfirst(x->x==indvec2[nn],indvec3)
+        # ket
+        vecmbindnntid[:,tid] .= in2bindmixedspins(Msize0,Np,indvec2,maxmatp21,matp20,matp21,vecmbindnn2tid[:,tid],vecmbindnntid[:,tid])
 
         for mm = 1:nn # down down up for bra
 
             # bra
-            # indvec2[mm] = (mmdown-1)*maxmatp21 + mmup
-            indbraup = mod(indvec2[mm],maxmatp21)
-            if indbraup != 0
-               indbradown = div(indvec2[mm],maxmatp21) + 1
-            else # indketup == 0
-               indbradown = div(indvec2[mm],maxmatp21)
-               indbraup = maxmatp21
-            end
-            in2bind!(indbradown,Msize0,Np-1,matp20,vecmbindmm2)
-            vecmbindmm[1:Np-1] = vecmbindmm2[1:Np-1]
-            in2bind!(indbraup,Msize0,1,matp21,vecmbindmm2)
-            vecmbindmm[Np:Np] = vecmbindmm2[1:1]
+            vecmbindmmtid[:,tid] .= in2bindmixedspins(Msize0,Np,indvec2,maxmatp21,matp20,matp21,vecmbindmm2tid[:,tid],vecmbindmmtid[:,tid])
 
-            # b = findfirst(x->x==indvec2[mm],indvec3)
+            vecindcoefftid[:,tid] .= coefficientInt2tid(vecmbindnntid[:,tid],vecmbindmmtid[:,tid],vecmbindnn3tid[:,tid],vecmbindmm3tid[:,tid],vecindcoefftid[:,:,tid],Np)
+            # ind0 = vecindcoefftid[1,3,tid]
+            # ind2 = Int64.(vecindcoefftid[1:ind0,1,tid])
+            # Hintdu[maxmatpcut+mm,maxmatpcut+nn] = sum(vecV[ind2].*vecindcoeff[1:ind0,2])
 
-            vecindcoeff, ind0 = coefficientInt2(vecmbindnn,vecmbindmm,vecmbindnn3,vecmbindmm3,vecindcoeff,Np)
-            ind2 = Int64.(vecindcoeff[1:ind0,1])
-            Hintdu[maxmatpcut+mm,maxmatpcut+nn] = sum(vecV[ind2].*vecindcoeff[1:ind0,2])
+            indrow_Hint2[binomial(nn,2)+mm] = mm
+            indcolumn_Hint2[binomial(nn,2)+mm] = nn
+            element_Hint2[binomial(nn,2)+mm] = sum(vecV[Int64.(vecindcoefftid[1:vecindcoefftid[1,3,tid],1,tid])].*vecindcoefftid[1:vecindcoefftid[1,3,tid],2,tid])
 
-            vecindcoeff, ind0 = coefficientInt3(vecmbindnn,vecmbindmm,vecmbindnn3,vecmbindmm3,vecindcoeff,Np)
-            ind2 = Int64.(vecindcoeff[1:ind0,1])
-            Hintdown[maxmatpcut+mm,maxmatpcut+nn] = sum(vecV[ind2].*vecindcoeff[1:ind0,2])
+            vecindcoefftid[:,tid] .= coefficientInt3tid(vecmbindnntid[:,tid],vecmbindmmtid[:,tid],vecmbindnn3tid[:,tid],vecmbindmm3tid[:,tid],vecindcoefftid[:,:,tid],Np)
+            # ind0 = vecindcoefftid[1,3,tid]
+            # ind2 = Int64.(vecindcoefftid[1:ind0,1,tid])
+            # Hintdown[maxmatpcut+mm,maxmatpcut+nn] = sum(vecV[ind2].*vecindcoeff[1:ind0,2])
 
-            # if !isa(a,Nothing) && !isa(b,Nothing)
-            #    vecindcoeff, ind0 = coefficientInt2(vecmbindnn,vecmbindmm,vecmbindnn3,vecmbindmm3,vecindcoeff,Np)
-            #    ind2 = Int64.(vecindcoeff[1:ind0,1])
-            #    Hintdu[maxmatpcut+mm,maxmatpcut+nn] = sum(vecV[ind2].*vecindcoeff[1:ind0,2])
-            # end
-            #
-            # if (!isa(a,Nothing) && !isa(b,Nothing)) || (isa(a,Nothing) && isa(a,Nothing))
-            #    vecindcoeff, ind0 = coefficientInt3(vecmbindnn,vecmbindmm,vecmbindnn3,vecmbindmm3,vecindcoeff,Np)
-            #    ind2 = Int64.(vecindcoeff[1:ind0,1])
-            #    Hintdown[maxmatpcut+mm,maxmatpcut+nn] = sum(vecV[ind2].*vecindcoeff[1:ind0,2])
-            # end
+            indrow_Hint[binomial(nn,2)+mm] = mm
+            indcolumn_Hint[binomial(nn,2)+mm] = nn
+            element_Hint[binomial(nn,2)+mm] = sum(vecV[Int64.(vecindcoefftid[1:vecindcoefftid[1,3,tid],1,tid])].*vecindcoefftid[1:vecindcoefftid[1,3,tid],2,tid])
 
         end
 
     end
 
-    # define Hint for down up up
-    # indNp = 2
-    # maxmatp31 = matp31[Msize0+1,2+1]
-    #
-    # for nn = 1:maxmatpcut2 # parfor
-    #
-    #     # ket
-    #     # indvec3[nn] = (nndown-1)*maxmatp31 + nnup
-    #     indketup = mod(indvec3[nn],maxmatp31)
-    #     if indketup != 0
-    #        indketdown = div(indvec3[nn],maxmatp31) + 1
-    #     else # indketup == 0
-    #        indketdown = div(indvec3[nn],maxmatp31)
-    #        indketup = maxmatp31
-    #     end
-    #     in2bind!(indketdown,Msize0,Np-indNp,matp30,vecmbindnn2)
-    #     vecmbindnn[1:Np-indNp] = vecmbindnn2[1:Np-indNp]
-    #     in2bind!(indketup,Msize0,indNp,matp31,vecmbindnn2)
-    #     vecmbindnn[Np-indNp+1:Np] = vecmbindnn2[1:indNp]
-    #
-    #     for mm = 1:nn
-    #
-    #         # bra
-    #         # indvec2[mm] = (mmdown-1)*maxmatp31 + mmup
-    #         indbraup = mod(indvec3[mm],maxmatp31)
-    #         if indbraup != 0
-    #            indbradown = div(indvec3[mm],maxmatp31) + 1
-    #         else # indketup == 0
-    #            indbradown = div(indvec3[mm],maxmatp31)
-    #            indbraup = maxmatp31
-    #         end
-    #         in2bind!(indbradown,Msize0,Np-indNp,matp30,vecmbindmm2)
-    #         vecmbindmm[1:Np-indNp] = vecmbindmm2[1:Np-indNp]
-    #         in2bind!(indbraup,Msize0,indNp,matp31,vecmbindmm2)
-    #         vecmbindmm[Np-indNp+1:Np] = vecmbindmm2[1:indNp]
-    #
-    #         vecindcoeff, ind0 = coefficientInt3(vecmbindnn,vecmbindmm,vecmbindnn3,vecmbindmm3,common[1:Np-2],vecindcoeff,Np)
-    #         ind2 = Int64.(vecindcoeff[1:ind0,1])
-    #         Hintdu[maxmatpcut+maxmatpcut2+mm,maxmatpcut+maxmatpcut2+nn] = sum(vecV[ind2].*vecindcoeff[1:ind0,2])
-    #
-    #     end
-    #
-    # end
+    indrow_Hint2 = Vector(indrow_Hint2)
+    indcolumn_Hint2 = Vector(indcolumn_Hint2)
+    element_Hint2 = Vector(element_Hint2)
+    deleteat!(indrow_Hint2, element_Hint2 .== 0.0)
+    deleteat!(indcolumn_Hint2, element_Hint2 .== 0.0)
+    deleteat!(element_Hint2, element_Hint2 .== 0.0)
+    Hintdu .= sparse(indrow_Hint2,indcolumn_Hint2,element_Hint2,maxmatpcut+maxmatpcut2*2+maxmatpcut,maxmatpcut+maxmatpcut2*2+maxmatpcut)
+
+    indrow_Hint = Vector(indrow_Hint)
+    indcolumn_Hint = Vector(indcolumn_Hint)
+    element_Hint = Vector(element_Hint)
+    deleteat!(indrow_Hint, element_Hint .== 0.0)
+    deleteat!(indcolumn_Hint, element_Hint .== 0.0)
+    deleteat!(element_Hint, element_Hint .== 0.0)
+    Hintdown .= Hintdown + sparse(indrow_Hint,indcolumn_Hint,element_Hint,maxmatpcut+maxmatpcut2*2+maxmatpcut,maxmatpcut+maxmatpcut2*2+maxmatpcut)
 
     # use conjectures for the lower triangle elements of Hint since it is hermite
     Hintdown .= Hintdown + Hintdown' - spdiagm(diag(Hintdown))
