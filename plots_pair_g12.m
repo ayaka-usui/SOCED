b126180450d8e690c4b84d34d2761b871b36b18a
Ayaka Usui <ayaka.usui@oist.jp>
2024-02-05 23:06:54 +0100

    edited

--- plots_pair_g12.m
+++ plots_pair_g12.m
@@ -1,13 +1,13 @@
 
 %%
-% clims = [0 0.5364];
+clims = [0 0.5364];
 
 %% 
 data=readmatrix('pair_TG.txt');
 xrange = data(:,1);
 yrange = data(:,2);
 fun = data(:,3:end);
-clims = [0 max(max(fun))];
+% clims = [0 max(max(fun))];
 figure(101)
 imagesc(xrange,yrange,fun,clims)
 pbaspect([1 1 1])
@@ -25,7 +25,7 @@ data=readmatrix('pair_g121_W26.txt');
 xrange = data(:,1);
 yrange = data(:,2);
 fun = data(:,3:end);
-clims = [0 max(max(fun))];
+% clims = [0 max(max(fun))];
 figure(102)
 imagesc(xrange,yrange,fun,clims)
 pbaspect([1 1 1])
@@ -40,8 +40,23 @@ data=readmatrix('pair_g1205_W26.txt');
 xrange = data(:,1);
 yrange = data(:,2);
 fun = data(:,3:end);
-clims = [0 max(max(fun))];
+% clims = [0 max(max(fun))];
 figure(103)
+imagesc(xrange,yrange,fun,clims)
+pbaspect([1 1 1])
+set(gca,'FontSize',28)
+set(gca, 'FontName', 'Times New Roman')
+set(gca,'YDir','normal')
+colorbar
+set(gca,'XTick',[], 'YTick', [])
+
+%%
+data=readmatrix('pair_g122_W26.txt');
+xrange = data(:,1);
+yrange = data(:,2);
+fun = data(:,3:end);
+% clims = [0 max(max(fun))];
+figure(114)
 imagesc(xrange,yrange,fun,clims)
 pbaspect([1 1 1])
 set(gca,'FontSize',28)
@@ -55,7 +70,7 @@ data=readmatrix('pair_g123_W26.txt');
 xrange = data(:,1);
 yrange = data(:,2);
 fun = data(:,3:end);
-clims = [0 max(max(fun))];
+% clims = [0 max(max(fun))];
 figure(104)
 imagesc(xrange,yrange,fun,clims)
 pbaspect([1 1 1])
@@ -70,7 +85,7 @@ data=readmatrix('pair_g121_W30.txt');
 xrange = data(:,1);
 yrange = data(:,2);
 fun = data(:,3:end);
-clims = [0 max(max(fun))];
+% clims = [0 max(max(fun))];
 figure(105)
 imagesc(xrange,yrange,fun,clims)
 pbaspect([1 1 1])
@@ -85,7 +100,7 @@ data=readmatrix('pair_g121_W20.txt');
 xrange = data(:,1);
 yrange = data(:,2);
 fun = data(:,3:end);
-clims = [0 max(max(fun))];
+% clims = [0 max(max(fun))];
 figure(106)
 imagesc(xrange,yrange,fun,clims)
 pbaspect([1 1 1])
@@ -100,7 +115,7 @@ data=readmatrix('pair_g121_W10.txt');
 xrange = data(:,1);
 yrange = data(:,2);
 fun = data(:,3:end);
-clims = [0 max(max(fun))];
+% clims = [0 max(max(fun))];
 figure(107)
 imagesc(xrange,yrange,fun,clims)
 pbaspect([1 1 1])
@@ -108,7 +123,7 @@ set(gca,'FontSize',28)
 set(gca, 'FontName', 'Times New Roman')
 set(gca,'YDir','normal')
 colorbar
-set(gca,'XTick',[], 'YTick', [])
+% set(gca,'XTick',[], 'YTick', [])
 
 %%
 
